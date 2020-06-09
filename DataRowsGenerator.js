298d3b7d151744dfa108a93af4ccfd426ebeb698
Samuel Beaulieu <sbeauli2@unb.ca>
2020-06-09 09:59:50 -0300

    Fixed heap out of memory issue on larger dumps

--- DataRowsGenerator.js
+++ DataRowsGenerator.js
@@ -1,138 +1,134 @@
-const fs = require('fs');
-const _7z = require('7zip-min');
-const convert = require('xml-js');
-
+var fs = require('fs');
+var _7z = require('7zip-min');
+var convert = require('xml-js');
 //Section 1: Choose Stackexchange dump, download and unzip
 //Main function
-if(process.argv.length !== 3) {
+if (process.argv.length !== 3) {
     console.log('Enter the name of the .7z stackdump file.');
     process.exit(0);
 }
 if (!fs.existsSync('./temp')) {
     fs.mkdirSync('./temp');
 }
-
-new Promise((resolve) => {
-    _7z.unpack('./' + process.argv[2], './temp', err => {
+new Promise(function (resolve) {
+    _7z.unpack('./' + process.argv[2], './temp', function (err) {
         resolve();
     });
-}).then(() => {
-    if(fs.readdirSync('./' + 'temp').length === 0) {
+}).then(function () {
+    if (fs.readdirSync('./' + 'temp').length === 0) {
         console.log('No file unzipped, exiting program.');
         cleanUp();
         process.exit(0);
     }
-
     console.log('Unzipping complete.');
     generateQueries();
 });
-
 //Section 2: Generate and execute queries
-function generateQueries(): void {
+function generateQueries() {
     //Create schema and get list of xml files
-    const xmlList: string[] = ['Users.xml', 'Badges.xml', 'Posts.xml', 'PostLinks.xml', 'Comments.xml', 'Tags.xml', 'PostHistory.xml', 'Votes.xml'];
-
-    const tempFiles: string[] = fs.readdirSync('./temp');
-    xmlList.forEach((xmlFile) => {
-        if(!tempFiles.includes(xmlFile)) {
+    var xmlList = ['Users.xml', 'Badges.xml', 'Posts.xml', 'PostLinks.xml', 'Comments.xml', 'Tags.xml', 'PostHistory.xml', 'Votes.xml'];
+    var tempFiles = fs.readdirSync('./temp');
+    xmlList.forEach(function (xmlFile) {
+        if (!tempFiles.includes(xmlFile)) {
             console.log('.7z did not contain the .xml files typical of a stack exchange data dump. Terminating program.');
             cleanUp();
             process.exit(0);
         }
     });
-
     generateDependencies();
     //Loop through files in folder
     console.log("Processing XML Files:");
-    for (let i = 0, p = Promise.resolve(); i < xmlList.length; i++) {
-        p = p.then(_ => new Promise(resolve => {
+    var _loop_1 = function (i, p) {
+        p = p.then(function (_) { return new Promise(function (resolve) {
             console.log('Processing ' + xmlList[i] + '...');
-            const columnList: string[] = generateColumns(xmlList[i].toLowerCase());
-            const dataTypes: string[] = generateTypes(xmlList[i].toLowerCase());
-            let splitText: string[] = [];
-
-            let myListener = newLineStream(function(line: string) {
-                if(line.length > 40) {
+            var columnList = generateColumns(xmlList[i].toLowerCase());
+            var dataTypes = generateTypes(xmlList[i].toLowerCase());
+            var splitText = [];
+            var myListener = newLineStream(function (line) {
+                if (line.length > 40) {
                     splitText.push(line);
-                    if(splitText.length === 10000) {
+                    if (splitText.length === 10000) {
                         processArray(splitText, xmlList[i], columnList, dataTypes);
                         splitText = [];
                     }
                 }
-            })
-            
-            fs.createReadStream(`./temp/${xmlList[i]}`, 'utf8')
+            });
+            fs.createReadStream("./temp/" + xmlList[i], 'utf8')
                 .on('data', myListener)
-                .on('end', () => {
-                    processArray(splitText, xmlList[i], columnList, dataTypes);
-                    if(i === xmlList.length - 1) {
-                        fs.appendFileSync('./dataRows.sql', '\nCOMMIT;');
-                        cleanUp();
-                    }
-                    resolve();
-                });
-        }));
+                .on('end', function () {
+                processArray(splitText, xmlList[i], columnList, dataTypes);
+                if (i === xmlList.length - 1) {
+                    fs.appendFileSync('./dataRows.sql', '\nCOMMIT;');
+                    cleanUp();
+                }
+                resolve();
+            });
+        }); });
+        out_p_1 = p;
+    };
+    var out_p_1;
+    for (var i = 0, p = Promise.resolve(); i < xmlList.length; i++) {
+        _loop_1(i, p);
+        p = out_p_1;
     }
-    
 }
-
 function newLineStream(callback) {
-	var buffer = '';
-	return (function (chunk) {
-		var i = 0, piece = '', offset = 0;
-		buffer += chunk;
-		while ((i = buffer.indexOf('\n', offset)) !== -1) {
-			piece = buffer.substr(offset, i - offset);
-			offset = i + 1;
-			callback(piece);
-		}
-		buffer = buffer.substr(offset);
-	});
+    var buffer = '';
+    return (function (chunk) {
+        var i = 0, piece = '', offset = 0;
+        buffer += chunk;
+        while ((i = buffer.indexOf('\n', offset)) !== -1) {
+            piece = buffer.substr(offset, i - offset);
+            offset = i + 1;
+            callback(piece);
+        }
+        buffer = buffer.substr(offset);
+    });
 }
-
-function processArray(splitText: string[], currentXml: string, columnList: string[], dataTypes: string[]) {
-    let query: string = '';
-    splitText.forEach((line) => {
-        const lowerFileName = currentXml.toLowerCase();
-        if(lowerFileName === 'posthistory.xml') {
+function processArray(splitText, currentXml, columnList, dataTypes) {
+    var query = '';
+    splitText.forEach(function (line) {
+        var lowerFileName = currentXml.toLowerCase();
+        if (lowerFileName === 'posthistory.xml') {
             query += 'SELECT stackdump.insert_' + lowerFileName.replace('.xml', '') + '(';
-        } else {
+        }
+        else {
             query += 'SELECT stackdump.insert_' + lowerFileName.replace('s.xml', '') + '(';
         }
-        
-        const jsonVer = convert.xml2js(line, {compact: true});
-        for(let j: number = 0; j < columnList.length; j++) {
-            let value: string = jsonVer.row._attributes[columnList[j]];
-            if(value) {
-                if(dataTypes[j] === 'TEXT') {
-                    value = value.replace(/\'/g,'\'\'');
-                    query += '\'' + value + '\'';
-                } else if(dataTypes[j] === 'INTEGER') {
-                    query += value;
-                } else if(dataTypes[j] === 'BOOLEAN') {
-                    query += value.toLowerCase();
-                } else {
+        var jsonVer = convert.xml2js(line, { compact: true });
+        for (var j = 0; j < columnList.length; j++) {
+            var value = jsonVer.row._attributes[columnList[j]];
+            if (value) {
+                if (dataTypes[j] === 'TEXT') {
+                    value = value.replace(/\'/g, '\'\'');
                     query += '\'' + value + '\'';
                 }
-            } else {
+                else if (dataTypes[j] === 'INTEGER') {
+                    query += value;
+                }
+                else if (dataTypes[j] === 'BOOLEAN') {
+                    query += value.toLowerCase();
+                }
+                else {
+                    query += '\'' + value + '\'';
+                }
+            }
+            else {
                 query += 'NULL';
             }
-
-            if(j !== columnList.length - 1) {
+            if (j !== columnList.length - 1) {
                 query += ', ';
             }
         }
         query += ');\n';
-        
     });
     fs.appendFileSync('./dataRows.sql', query);
 }
-
 /*
 Generates the requires lines of the output file.
 */
-function generateDependencies(): void {
-    let query: string = '-- Deploy stackdump:dataRows to pg\n';
+function generateDependencies() {
+    var query = '-- Deploy stackdump:dataRows to pg\n';
     query += '-- requires: appschema\n';
     query += '-- requires: badges\n';
     query += '-- requires: comments\n';
@@ -152,16 +148,14 @@ function generateDependencies(): void {
     query += '-- requires: insert_vote\n';
     query += '\n';
     query += 'BEGIN;\n\n';
-
     fs.writeFileSync('./dataRows.sql', query);
 }
-
 /*
 Generates a column list that contains the names of each column.
 */
-function generateColumns(currentXml: string): string[] {
-    let columnList: string[] = [];
-    switch(currentXml) {
+function generateColumns(currentXml) {
+    var columnList = [];
+    switch (currentXml) {
         case 'users.xml': {
             columnList = ['Id', 'Reputation', 'CreationDate', 'DisplayName', 'LastAccessDate', 'WebsiteUrl'];
             columnList.push('Location', 'AboutMe', 'Views', 'UpVotes', 'DownVotes', 'ProfileImageUrl', 'AccountId');
@@ -201,14 +195,12 @@ function generateColumns(currentXml: string): string[] {
     }
     return columnList;
 }
-
 /*
 Generates a dataTypes: number[] array that contains the data type of each key.
 */
-function generateTypes(currentXml: string): string[] {
-    let dataTypes: string[] = [];
-
-    switch(currentXml) {
+function generateTypes(currentXml) {
+    var dataTypes = [];
+    switch (currentXml) {
         case 'users.xml': {
             dataTypes = ['INTEGER', 'INTEGER', 'TIMESTAMP', 'TEXT', 'TIMESTAMP', 'TEXT', 'TEXT', 'TEXT', 'INTEGER'];
             dataTypes.push('INTEGER', 'INTEGER', 'TEXT', 'INTEGER');
@@ -245,11 +237,10 @@ function generateTypes(currentXml: string): string[] {
     }
     return dataTypes;
 }
-
 /*
 Deletes .xml, .sql and the directory used to hold those files after the query has run.
 */
-function cleanUp(): void {
-    fs.readdirSync('./temp').forEach((file) => fs.unlinkSync('./temp/' + file));
+function cleanUp() {
+    fs.readdirSync('./temp').forEach(function (file) { return fs.unlinkSync('./temp/' + file); });
     fs.rmdirSync('./temp');
 }
